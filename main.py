97986ec3051a675171b40e02ea2cc2761e937d86
Seple <armani1984snajpi@gmail.com>
2025-02-07 16:48:32 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,47 +7,80 @@ import datetime
 # Linki do pobrania list
 urls = [
     # 1Hosts - Różne poziomy filtracji
+    "https://raw.githubusercontent.com/badmojr/1Hosts/refs/heads/master/mini/adblock.txt",
     "https://raw.githubusercontent.com/badmojr/1Hosts/refs/heads/master/Lite/adblock.txt",
     "https://raw.githubusercontent.com/badmojr/1Hosts/refs/heads/master/Pro/adblock.txt",
     "https://raw.githubusercontent.com/badmojr/1Hosts/refs/heads/master/Xtra/adblock.txt",
-    "https://raw.githubusercontent.com/badmojr/1Hosts/refs/heads/master/mini/adblock.txt",
-    # Filtry AdGuard
-    "https://adguardteam.github.io/HostlistsRegistry/assets/filter_1.txt",
-    "https://adguardteam.github.io/HostlistsRegistry/assets/filter_4.txt",
     # Hagezi - Różne poziomy filtracji
     "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/adblock/light.txt",
     "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/adblock/multi.txt",
     "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/adblock/pro.txt",
     "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/adblock/pro.plus.txt",
     "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/adblock/ultimate.txt",
-    # Kolejne filtry AdGuard
+    # OISD - Różne poziomy filtracji
     "https://adguardteam.github.io/HostlistsRegistry/assets/filter_5.txt",
     "https://adguardteam.github.io/HostlistsRegistry/assets/filter_27.txt",
+    # Peter Lowe's Blocklist
     "https://adguardteam.github.io/HostlistsRegistry/assets/filter_3.txt",
+    # Steven Black's List
     "https://adguardteam.github.io/HostlistsRegistry/assets/filter_33.txt",
+    # Dan Pollock's List
+    "https://adguardteam.github.io/HostlistsRegistry/assets/filter_4.txt",
+    # AdGuard DNS filter
+    "https://adguardteam.github.io/HostlistsRegistry/assets/filter_1.txt",
+    # AWAvenue Ads Rule
+    "https://adguardteam.github.io/HostlistsRegistry/assets/filter_53.txt",
+    # Malicious URL Blocklist (URLHaus)
     "https://adguardteam.github.io/HostlistsRegistry/assets/filter_11.txt",
+    # uBlock filters – Badware risks
     "https://adguardteam.github.io/HostlistsRegistry/assets/filter_50.txt",
+    # The Big List of Hacked Malware Web Sites
     "https://adguardteam.github.io/HostlistsRegistry/assets/filter_9.txt",
+    # Stalkerware Indicators List
     "https://adguardteam.github.io/HostlistsRegistry/assets/filter_31.txt",
+    # ShadowWhisperer's Malware List
     "https://adguardteam.github.io/HostlistsRegistry/assets/filter_42.txt",
+    # Scam Blocklist by DurableNapkin
     "https://adguardteam.github.io/HostlistsRegistry/assets/filter_10.txt",
-    "https://adguardteam.github.io/HostlistsRegistry/assets/filter_30.txt",
+    # NoCoin Filter List
+    "https://adguardteam.github.io/HostlistsRegistry/assets/filter_8.txt",
+    # Dandelion Sprout's Anti-Malware List
     "https://adguardteam.github.io/HostlistsRegistry/assets/filter_12.txt",
+    # Phishing URL Blocklist (PhishTank and OpenPhish)
+    "https://adguardteam.github.io/HostlistsRegistry/assets/filter_30.txt",
+    # POL: Polish filters for Pi-hole
+    "https://adguardteam.github.io/HostlistsRegistry/assets/filter_14.txt",
+    # POL: CERT Polska List of malicious domains
+    "https://adguardteam.github.io/HostlistsRegistry/assets/filter_41.txt",
+    # WindowsSpyBlocker - Hosts spy rules
+    "https://adguardteam.github.io/HostlistsRegistry/assets/filter_23.txt",
+    # Dandelion Sprout's Anti Push Notifications
+    "https://adguardteam.github.io/HostlistsRegistry/assets/filter_39.txt",
+    # Dandelion Sprout's Game Console Adblock List)
+    "https://adguardteam.github.io/HostlistsRegistry/assets/filter_6.txt",
+    # Perflyst and Dandelion Sprout's Smart-TV Blocklist
+    "https://adguardteam.github.io/HostlistsRegistry/assets/filter_7.txt",
 ]
 
 # Lista wykluczeń
 exclude_list = {
+    # Aplikacja Facebook/Messenger na Androida/iOS (komentarze, wiadomości)
+    # Filmy z historiami aplikacji na Instagramie
     "b-graph-fallback.facebook.com",
     "b-graph.facebook.com",
     "graph-fallback.facebook.com",
     "graph.facebook.com",
     "graph.fbpigeon.com",
     "z-m-graph.facebook.com",
+    # Aplikacja Facebook Lite (wiadomości/powiadomienia), ustawienia aplikacji Instagram (wolne ładowanie)
+    # Aplikacja Facebook/Messenger na Androida/iOS (Awatary)
     "web.facebook.com",
     "web-fallback.facebook.com",
+    # Inne funkcje Facebooka, w tym logowanie 
     "connect.facebook.com",
     "connect.facebook.net",
     "mqtt-mini.facebook.com",
+    # Gry i Aplikacje Android
     "mycafe.games",
     "tinyco.com",
     "shephertz.com",
@@ -58,6 +91,7 @@ exclude_list = {
     "v-speed.eu",
     "schwarz",
     "weathercn.com",
+    # Google
     "googletagmanager.com",
     "adservice.google",
     "googleadservices.com",
@@ -129,3 +163,4 @@ try:
     print(f"📊 Podsumowanie: Pobranie: {total_downloaded} reguł, Usunięte (duplikaty i subdomeny): {removed_rules} reguł, Pozostałe unikalne po usunięciu duplikatów i subdomen: {len(all_rules)} reguł")
 except Exception as e:
     print(f"❌ Błąd zapisu pliku: {e}")
+    
