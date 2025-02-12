8c089346c626e5e7e73fc7a491f46cfad49a109d
Seple <armani1984snajpi@gmail.com>
2025-02-12 23:56:22 +0100

    Update main.py

--- main.py
+++ main.py
@@ -97,7 +97,7 @@ urls = [
     # HaGeZi's Threat Intelligence Feeds DNS Blocklist
     "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/adblock/tif.txt",
     # Native Tracker
-    "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/adblock/nosafesearch.txt",
+    "https://raw.githubusercontent.com/Seple/Full_DNS_Block/refs/heads/main/Optimization_suggestion.txt",
 ]
 
 # Lista wykluczeń
