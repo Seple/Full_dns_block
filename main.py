912b836b8d860448e42077e1d5352c0512252705
Seple <armani1984snajpi@gmail.com>
2025-02-12 11:41:05 +0100

    Update main.py

--- main.py
+++ main.py
@@ -91,8 +91,10 @@ urls = [
     "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/adblock/hoster.txt",
     # HaGeZi's safesearch not supported DNS Blocklist
     "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/adblock/nosafesearch.txt",
-        # HaGeZi's The World's Most Abused TLDs
-    "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/adblock/nosafesearch.txt",
+    # HaGeZi's The World's Most Abused TLDs
+    "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/adblock/spam-tlds-adblock.txt",
+        # HaGeZi's Threat Intelligence Feeds DNS Blocklist
+    "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/adblock/tif.txt",
         # Native Tracker
     "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/adblock/nosafesearch.txt",
 ]
